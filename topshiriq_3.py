45b9aab224f9c716f4fba84e8670f7817a077a6d
cuxay <cuxayl@gmail>
2025-05-22 11:40:58 +0500

    message

--- topshiriq_3.py
+++ topshiriq_3.py
@@ -4,3 +4,6 @@ import requests
 https://jsonplaceholder.typicode.com/todos/1 sahifasiga so'rov yuboring
 va JSON javobini chiqarib bering.
 """
+url = 'https://jsonplaceholder.typicode.com/todos/1'
+r = requests.get(url)
+print(r.json())
--- topshiriq_4.py
+++ topshiriq_4.py
@@ -4,3 +4,9 @@ import requests
 3-topshiriqdagi JSON javobdan "title" maydonini
 ajratib olib konsolga chiqaring.
 """
+url ='https://jsonplaceholder.typicode.com/todos/1'
+r = requests.get(url)
+d = r.json()
+
+
+print(d['title'])
