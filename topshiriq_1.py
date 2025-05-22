07f8cdb277600c7e102d6b662c8da99918340bcb
cuxay <cuxayl@gmail>
2025-05-22 11:32:19 +0500

    xabar birinchi ishlash

--- topshiriq_1.py
+++ topshiriq_1.py
@@ -4,3 +4,6 @@ import requests
 https://example.com sahifasiga GET so'rov yuboring
 va javob holatini (status_code) ekranga chiqaring.
 """
+url = 'https://example.com'
+r = requests.get(url)
+print(r.status_code)
--- topshiriq_2.py
+++ topshiriq_2.py
@@ -4,3 +4,6 @@ import requests
 https://example.com sahifasiga GET so'rov yuboring
 va javob matnini (text) konsolga chiqaring.
 """
+url = 'https://example.com'
+r = requests.get(url)
+print(r.text)
